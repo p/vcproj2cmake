db74bce0d8a56ddf918589289457e33e4108f0ba
Andreas Mohr <andim2@users.sourceforge.net>
2013-04-01 09:40:21 +0200

    Tests: add script for full round-trip testing (convert / configure / build / run).

---
+++ tests/run_all_tests.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+class CommandExecutionError < StandardError
+  def initialize(str_cmdline, exitstatus, output)
+    @str_cmdline = str_cmdline
+    @exitstatus = exitstatus
+    @output = output
+  end
+  def message
+    "Command execution failed. Command line #{@str_cmdline}, exit status #{@exitstatus.to_s}, output #{@output}."
+  end
+end
+
+def execute_command(arr_cmdline)
+  str_cmdline = arr_cmdline.join(' ')
+  #puts "Executing command line #{str_cmdline}"
+  output = `#{str_cmdline}`
+  if not $?.success?
+    raise CommandExecutionError(str_cmdline, $?.exitstatus, output)
+  end
+end
+
+script_dir_rel = File.dirname(__FILE__)
+script_dir = File.expand_path(script_dir_rel)
+
+conv_bin = script_dir + '../../scripts/vcproj2cmake.rb'
+
+cmake_source_dir = script_dir
+
+output_convert = `#{conv_bin} #{cmake_source_dir} 2>&1`
+
+if not $?.success?
+  test_failed_st_reason = "script indicated failure (exit code: #{$?.exitstatus})"
+end
+
+cmake_build_dir = "#{cmake_source_dir}/../st.build"
+begin
+  Dir.mkdir(cmake_build_dir)
+rescue Errno::EEXIST
+  # ignore it
+end
+
+Dir.chdir(cmake_build_dir) do |path|
+  execute_command([ 'cmake', '-DCMAKE_BUILD_TYPE=Debug', cmake_source_dir ])
+  # FIXME: tap into CMAKE_MAKE_PROGRAM. Could move the mechanism used by our
+  # installer script into a common module.
+  execute_command([ 'make' ])
+  execute_command([ './projects_full/vcxproj_full_1/vcxproj_full_1' ])
+end
