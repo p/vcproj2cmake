7b50e7b099050da2ccb05ff193a793049894c7ee
Andreas Mohr <andim2@users.sourceforge.net>
2013-04-24 05:27:22 +0200

    ONGOING: Downloader work.

--- tools/download_projects_random/download_projects_random_multi.sh
+++ tools/download_projects_random/download_projects_random_multi.sh
@@ -4,11 +4,12 @@ set -x
 
 for i in $(seq 1 10); do
   dat=$(date +%s)
-  log=/tmp/download_projects_random_${dat}.log
+  dldir=dl_projects_random.tmp
+  mkdir "${dldir}"
+  log="${dldir}/download_projects_random_${dat}.log"
   echo "Logging to ${log}"
-  ./download_projects_random.rb >${log} 2>&1 || {
+  ./download_projects_random.rb "${dldir}" >${log} 2>&1 || {
     echo "FAIL ${dat}"
-    dldir=dl_projects_random.tmp
     mv ${dldir} ${dldir}.FAIL_${dat}
   }
 done
