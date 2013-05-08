913ab497a0b7bbe1449c1da43d44ce8267922c06
Andreas Mohr <andim2@users.sourceforge.net>
2013-05-08 23:19:13 +0200

    File list generator: set target property VS_DOTNET_REFERENCES.

--- scripts/lib/vcproj2cmake/v2c_core.rb
+++ scripts/lib/vcproj2cmake/v2c_core.rb
@@ -6927,8 +6927,10 @@ class V2C_CMakeProjectTargetGenerator < V2C_CMakeV2CSyntaxGenerator
     # Yup, this stuff should perhaps be shoved into
     # write_project_target_config(), too.
     if target_is_valid
-      arr_config_info = project_info.arr_config_info
       when_target_valid_scriptlet_block(@target.name) {
+        put_dotnet_references()
+
+        arr_config_info = project_info.arr_config_info
         arr_config_info.each { |config_info_curr|
           condition = config_info_curr.condition
 
@@ -7055,6 +7057,17 @@ class V2C_CMakeProjectTargetGenerator < V2C_CMakeV2CSyntaxGenerator
   end
 
   private
+  def put_dotnet_references()
+    list_references = @target.file_lists.lookup_from_list_type(V2C_File_List_Info::TYPE_CS_REFERENCE)
+    return if list_references.nil?
+    arr_references = array_collect_compact(list_references.arr_files) do |file|
+      file_info.path_relative
+    end
+    put_property_dotnet_references(@target.name, arr_references)
+  end
+  def put_property_dotnet_references(target_name, arr_references)
+    set_property(target_name, PROP_SET, 'VS_DOTNET_REFERENCES', arr_references)
+  end
   def put_project(project_name, arr_progr_languages = nil)
     arr_args_project_name_and_attrs = [ project_name ]
     if arr_progr_languages.nil? or arr_progr_languages.empty?
