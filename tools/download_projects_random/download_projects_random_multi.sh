07b91fda152aced33038231fcd4803219a71f1c9
Andreas Mohr <andim2@users.sf.net>
2013-07-07 23:39:03 +0200

    Downloader: misc. improvements.
    - make download target directory configurable
    - rename away directory on success as well
    - move implementation into helpers
    - add package installation hint for the apparently less widespread Nokogiri dependency

--- tools/download_projects_random/download_projects_random_multi.sh
+++ tools/download_projects_random/download_projects_random_multi.sh
@@ -3,12 +3,20 @@
 set -x
 
 for i in $(seq 1 10); do
-  dat=$(date +%s)
-  log=/tmp/download_projects_random_${dat}.log
+  dat="$(date +%s)"
+  dldir="${DL_DIR:-dl_projects_random.tmp}"
+  mkdir "${dldir}"
+  log="${dldir}/download_projects_random_${dat}.log"
   echo "Logging to ${log}"
-  ./download_projects_random.rb >${log} 2>&1 || {
+  # Using tee would be nice, but that greatly complicates exit code handling,
+  # thus choose to not do that for now...
+  #./download_projects_random.rb "${dldir}" 2>&1 | tee ${log}
+  ./download_projects_random.rb "${dldir}" 2>&1 > ${log}
+  #ls -l "${log}"; cat "${log}"
+  if [ "$?" != "0" ]; then
     echo "FAIL ${dat}"
-    dldir=dl_projects_random.tmp
     mv ${dldir} ${dldir}.FAIL_${dat}
-  }
+  else
+    mv ${dldir} ${dldir}.ok_${dat}
+  fi
 done
